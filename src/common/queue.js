d2e2b221c7bcb5b105d077edea2359e787611e86
LA Roberto <louise.roberto.1337@gmail.com>
2016-06-30 14:38:53 +0800

    adjust redis connection string

--- src/common/queue.js
+++ src/common/queue.js
@@ -4,8 +4,6 @@ var util = require('util');
 var Queue = require('bull');
 var params = require('../../app/config/parameters').redis;
 
-var waybillSendQueue = Queue('Waybill Request Queue', {
-    url: util.format('redis://%s:%s/%s', params.host, params.port, params.db_number)
-});
+var waybillSendQueue = Queue('Waybill Request Queue', params.port, params.host);
 
 module.exports.waybillQueue = waybillSendQueue;
